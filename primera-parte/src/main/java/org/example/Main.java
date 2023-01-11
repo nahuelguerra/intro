1ede660b4c657eff20866c18317ea19c12e2253d
nahuelguerra <nahuelguerra997@gmail.com>
2023-01-11 16:21:52 -0300

    ejercicios terminados

---
+++ primera-parte/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int result = suma(10, 30, 40);
+        System.out.println(result);
+    }
+
+    public  static int suma(int a, int b, int c){
+        return a + b + c;
+    }
+}
---
+++ segunda-parte/src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Coche miCoche = new Coche();
+        miCoche.SumarPuerta();
+        miCoche.SumarPuerta();
+        miCoche.SumarPuerta();
+        System.out.println(miCoche.puertas);
+    }
+}
+
+class Coche{
+    public int puertas = 0;
+
+    public void SumarPuerta(){
+        this.puertas++;
+    }
+}
